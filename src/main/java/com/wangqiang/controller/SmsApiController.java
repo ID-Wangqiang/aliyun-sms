fe74bb89a130ff4f75cdd98df8bf1b7e057f1ca4
wangqiang <wangqiang1996106@gmail.com>
2021-12-25 15:46:50 +0800

    mod:更新前端资源

--- src/main/java/com/wangqiang/controller/SmsApiController.java
+++ src/main/java/com/wangqiang/controller/SmsApiController.java
@@ -1,14 +1,13 @@
 package com.wangqiang.controller;
 
 import com.wangqiang.service.SendSms;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
@@ -23,6 +22,8 @@ import java.util.concurrent.TimeUnit;
 @RestController
 @CrossOrigin //处理跨域问题
 public class SmsApiController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SmsApiController.class);
 
     @Value("${my.sms.randomCodeNumber}")
     private String randomCodeNumber;
@@ -43,14 +44,14 @@ public class SmsApiController {
     private RedisTemplate<String,String> redisTemplate;
 
 
-    @GetMapping("/send/{phone}")
-    public String code(@PathVariable("phone")String phone){
+    @RequestMapping("/send")
+    public String code(String phone){
 
         String code = redisTemplate.opsForValue().get(phone);
-        //判断redis缓存中是否有手机号验证码的记录
-        if (!StringUtils.isEmpty(code)){
-            return phone + ":" + code + "已存在，还没有过期,2分钟过期";
-        }
+//        //判断redis缓存中是否有手机号验证码的记录
+//        if (!StringUtils.isEmpty(code)){
+//            return phone + ":" + code + "已存在，还没有过期,2分钟过期";
+//        }
 
         //生成纯6位数字验证码并存储redis
         code = String.valueOf((Math.random() * 9 + 1) * 1000000).toString().substring(0, Integer.valueOf(randomCodeNumber));
@@ -66,33 +67,40 @@ public class SmsApiController {
         // 阿里云短信发送服务
         HashMap<String, Object> map = new HashMap<>();
         map.put("code",code);
-        boolean isSend = sendSms.send(phone, map);
 
-        if (isSend){
+        logger.warn("手机号："+phone+" 验证码：" + code);
+        // 暂时不引用阿里云服务
+//        boolean isSend = sendSms.send(phone, map);
+
+        if (true){
             // 验证码有效期分钟
             redisTemplate.opsForValue().set(phone,code, Long.valueOf(expiryTime),TimeUnit.MINUTES);
             // 每个手机号60秒只能发送1次验证码
-            redisTemplate.opsForValue().set(smsPhone,null,Long.valueOf(sendOneExistTime),TimeUnit.SECONDS);
+            redisTemplate.opsForValue().set(smsPhone,smsPhone,Long.valueOf(sendOneExistTime),TimeUnit.SECONDS);
             // 每个验证码只能使用3次
             String phoneCount = "COUNT" + phone;
             redisTemplate.opsForValue().set(phoneCount,maxUseCount,2,TimeUnit.MINUTES);
-            return phone + ":" + code + "发送成功！";
+            String json = "{\"message\":"+true+",\"sms\":\""+code+"\"}";
+            return json;
         }else {
             return "发送失败！";
         }
 
     }
 
-    @GetMapping("/vaile/{phone}/{code}")
-    public String vaile(@PathVariable("phone")String phone,@PathVariable("code")String code){
+    @GetMapping("/validate")
+    public String vaile(String phone,String code){
         String phoneCode = redisTemplate.opsForValue().get(phone);
         //判断redis缓存中是否有手机号验证码的记录
-        if (!StringUtils.isEmpty(phoneCode)){
-            return phone + ":" + phoneCode + "手机号验证码已过期，请重新发送验证码";
+        String json = "{\"message\":"+false+"}";
+        if (StringUtils.isEmpty(phoneCode)){
+            logger.warn("手机号：" +phoneCode+ ",验证码已过期，请重新发送验证码");
+            return json;
         }
         // 判断验证码是否正确
         if (!code.equals(phoneCode)) {
-            return phone+ ":" +phoneCode + "输入验证码有误，请重新输入";
+            logger.warn("手机号：" +phoneCode+ ",输入验证码有误，请重新输入");
+            return json;
         }else {
             // 验证码可验证次数减少
             String phoneCount = "COUNT" + phone;
@@ -101,9 +109,12 @@ public class SmsApiController {
             if (count > 0) {
                 count--;
                 redisTemplate.opsForValue().set(phoneCount,String.valueOf(count),2,TimeUnit.MINUTES);
-                return phone+ ":" +phoneCode + "验证次数还有" +count+ "次";
+                logger.warn("手机号：" +phoneCode+ ",验证次数还有" +count+ "次");
+                json = "{\"message\":"+true+"}";
+                return json;
             }else {
-                return phone+ ":" +phoneCode + "验证成功";
+                logger.warn("手机号：" +phoneCode+ ",只能验证三次");
+                return json;
             }
         }
     }
